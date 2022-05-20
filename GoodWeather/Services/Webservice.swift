e9bf33d09de113bac83f5f96fe59fc161d3a522b
Kris Mitina <kristmitina@gmail.com>
2022-05-20 19:33:12 +0200

    Implementing WebService Service

---
+++ GoodWeather/Services/Webservice.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  GoodWeather
+//
+//  Created by DVKSH on 20.05.22.
+//
+
+import Foundation
+
+struct Resource<T> {
+    let url: URL
+    let parse: (Data) -> T?
+}
+
+final class Webservice {
+    
+    func load<T>(resource: Resource<T>, completion: @escaping (T?) -> ()) {
+        
+        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+            
+            if let data = data {
+                DispatchQueue.main.async {
+                    completion(resource.parse(data))
+                }
+            } else {
+                completion(nil)
+            }
+            
+        }.resume()
+    }
+    
+}
