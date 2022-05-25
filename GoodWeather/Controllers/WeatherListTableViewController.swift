be590a488808497056a920772c6837c5820f385b
Kris Mitina <kristmitina@gmail.com>
2022-05-25 15:52:02 +0200

    Decoding JSON into Models

--- GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -13,6 +13,17 @@ class WeatherListTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let resource = Resource<WeatherResponse>(url: URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=8b0a472e6b603a9a8cdc2949130fe873")!) { data in
+            
+           return try? JSONDecoder().decode(WeatherResponse.self, from: data)
+        }
+        
+        Webservice().load(resource: resource) { weatherResponse in
+            if let weatherResponse = weatherResponse {
+                print(weatherResponse)
+            }
+        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
