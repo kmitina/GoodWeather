71a34dcbc4375b03f2a00d94cda5f3384932caf9
Kris Mitina <kristmitina@gmail.com>
2022-05-31 17:48:20 +0200

    Displaying Temperature Units on Settings Screen

--- GoodWeather/Controllers/SettingTableViewController.swift
+++ GoodWeather/Controllers/SettingTableViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class SettingTableViewController: UITableViewController {
     
+    private var settingsVoewModel = SettingsViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsVoewModel.units.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let settingsItem = settingsVoewModel.units[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
+        cell.textLabel?.text = settingsItem.displayName
+        return cell 
+    }
 }
